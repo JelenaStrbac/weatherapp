6acf320c9e83e201176b3306ba537869b35a09b6
Jelena Strbac <strbac.jelena.js@gmail.com>
2020-03-02 16:22:12 +0100

    2nd commit

--- script.js
+++ script.js
@@ -1,51 +1,250 @@
-let weath = [];
+let weather = [];
+
+let state = {
+    lastUpdate: 0
+};
+
+// LOADER functions
+function renderLoader() {
+    const loader = document.getElementById('loader');
+    loader.style.display = 'block';
+};
+
+function clearLoader() {
+    const loader = document.getElementById('loader');
+    loader.style.display = 'none';
+};
+
+// render weather icons
+function renderWeatherIcons() {
+    let src;
+    const descr = weather.current.weather_descriptions[0];
+    const isDay = weather.current.is_day;
+
+    switch (descr) {
+        case 'Sunny':
+        case 'Clear':
+            isDay === 'no' ? src = 'moon.svg' : src = 'sun.svg';
+            break;
+        case 'Overcast':
+            isDay === 'no' ? src = 'moon.svg' : src = 'cloud.svg';
+            break;
+        case 'Partly cloudy':
+            isDay === 'no' ? src = 'moon.svg' : src = 'cloudy.svg';
+            break;
+        case 'Rain':
+        case 'Light Rain':
+            src = 'rain.svg';
+            break;
+        case 'Storm':
+            src = 'storn.svg';
+            break;
+        case 'Snow':
+            src = 'snowing.svg';
+            break;
+        default:
+            src = '';
+    }
+    return src;
+}
 
 
-function enterCity(e) {
-    e.preventDefault();
+// dynamic background
+function dynamicBckg() {
+    let bckg;
+    const descr = weather.current.weather_descriptions[0];
+    const isDay = weather.current.is_day;
 
-    document.getElementById('loader').style.display = 'block'; 
+    switch (descr) {
+        case 'Sunny':
+        case 'Clear':
+            isDay === 'no' ? bckg = 'night-clear.jpg' : bckg = 'sunny.jpg';
+            break;
+        case 'Overcast':
+            isDay === 'no' ? bckg = 'night-cloud.jpg' : bckg = 'overcast.jpg';
+            break;
+        case 'Partly cloudy':
+            isDay === 'no' ? bckg = 'night-cloud.jpg' : bckg = 'partlycloudy.jpg';
+            break;
+        case 'Rain':
+        case 'Light Rain':
+            isDay === 'no' ? bckg = 'night-rain.jpg' : bckg = 'rain.jpg';
+            break;
+        case 'Storm':
+            isDay === 'no' ? bckg = 'night-storm.jpg' : bckg = 'storm.jpg';
+            break;
+        case 'Snow':
+            bckg = 'snow.jpg';
+            break;
+        default:
+            bckg = '';
+    }
 
-    const loc = document.getElementById('iCity').value;
-    console.log(loc);
+    document.body.style.backgroundImage = `url('background/${bckg}')`;
+    document.body.style.backgroundSize = 'cover';
+};
+
+
+// current date
+function currentDate() {
+    return new Date().toLocaleDateString();
+};
+
+// rendering elements
+function renderElements() {
+    const showAll = document.getElementById('wrapper');
+    showAll.style.display = 'flex';
+    showAll.innerHTML = `        
+            <div class="main">
+                <div class="city">${weather.location.name}, ${weather.location.country}</div>
+                <div class="date">${currentDate()}</div>
+                <div class="degree">${weather.current.temperature}°</div>
+                <img id="weather-icon" src="icons/${renderWeatherIcons()}">
+                <div class="description">${weather.current.weather_descriptions}</div>
+            </div>
+            <div>
+                <div class="wpp">
+                    <div class="wpp-small">
+                        <div class="info-item">
+                            <span class="info-m"><i class="fas fa-wind"></i>${weather.current.wind_speed} kmph</span>
+                            <span class="info-text"></i>Wind</span>
+                        </div>
+
+                        <div class="info-item">
+                            <span class="info-m"><i class="fas fa-tint"></i>${weather.current.precip} mm</span>
+                            <span class="info-text">Precipitation</span>
+                        </div>
+                    </div>
+
+                    <div class="wpp-small">
+                        <div class="info-item">
+                            <span class="info-m"><i class="far fa-clock"></i>${weather.current.pressure} mb</span>
+                            <span class="info-text">Pressure</span>
+                        </div>
+
+                        <div class="info-item">
+                            <span class="info-m"><i class="fas fa-water"></i>${weather.current.humidity} %</span>
+                            <span class="info-text">Humidity</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="feels-like">
+                    <div class="feels-like-small">
+                        <span class="info-m"><i class="fas fa-temperature-low"></i>${weather.current.feelslike}°</span>
+                        <span class="info-text"></i>Feels like</span>
+                    </div>
+                    <div><img class="refresh-btn" src="icons/refresh.svg" alt="refresh"> Updated <strong>${start()} ${start() > 0 ? 'min' : 'mins'}</strong>
+                        ago
+                    </div>
+                </div>
+            </div>
+                    `
+};
+
+
+// clear elements
+function clearElements() {
+    const showAll = document.getElementById('wrapper');
+    showAll.innerHTML = '';
+};
+
+//display weather on UI
+function showWeather() {
+
+    // remove loader and elements
+    clearLoader();
     
-    fetch(`http://api.weatherstack.com/current?access_key=3d067e465fea96840b85e108ffc5979d&query=${loc}`)
+    // display dynamic background
+    dynamicBckg();
+
+    // update state 
+    state.lastUpdate = Date.now();
+    
+    // render elements
+    renderElements();
+       
+};
+
+
+
+// CURRENT IP LOCATION function
+(function loadCurrentLoc() {
+    // show loader
+    renderLoader();
+    clearElements();
+
+    // fetching data - current IP address
+    fetch(`http://api.weatherstack.com/current?access_key=3d067e465fea96840b85e108ffc5979d&query=fetch:ip`)
     .then((response) => {
-        // if (response.status === 200) {
-        //     return response;
-        // } else {
-        //     throw new Error('Response not valid');
-        // }
         return response.json();
     })
-    .then((weather) => {
-        weath = weather;
+    .then((data) => {
+        weather = data;
         showWeather();
     })
     .catch(e => {
         console.log(e);
     });
+})();
+
+
+// SEARCHING LOCATION function
+function enterCity(e) {
+    e.preventDefault();
+
+    // show loader
+    renderLoader();
+    clearElements();
+
+    // get location from input field
+    const loc = document.getElementById('iCity').value;
+
+    // fetching data - searching location
+    fetch(`http://api.weatherstack.com/current?access_key=3d067e465fea96840b85e108ffc5979d&query=${loc}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        weather = data;
+        showWeather();
+    })
+    .catch(e => {
+        console.log(e);
+    }); 
+};
+
+// event listeners
+document.getElementById('fSearch').addEventListener('submit', enterCity);
+document.getElementById('wrapper').addEventListener('click', e => {
+    const btn = e.target.matches('.refresh-btn, .refresh-btn *'); 
     
+    if (btn) {
+        showWeather();
+    }
+});
+
+// console.log(state.lastUpdate);
+
+// UPDATE TIME function
+function updateTime() {
+    let now = Date.now();
+    let diff;
+    
+    diff = Math.floor((now - state.lastUpdate) / (1000 * 60));
+    return diff;
+};
+
+// test = updateTime()
+// console.log(test);
+// console.log(state.lastUpdate);
+
+function start() {
+    return setInterval(updateTime, 1000);
 }
 
-
-function showWeather() {
-
-    document.getElementById('loader').style.display = 'none';
-    const showAll = document.getElementById('wrapper');
-    showAll.style.display = 'flex';
-    showAll.innerHTML = `<div>
-                            <div>${weath.location.name}, ${weath.location.country}</div>
-                            <div>${weath.current.temperature}°C</div>
-                            <img src="${weath.current.weather_icons}">
-                            <div>${weath.current.weather_descriptions}</div>
-                        </div>
-                        <div>
-                            <div>Wind: ${weath.current.wind_speed} kmph</div>
-                            <div>Precip: ${weath.current.precip} mm</div>
-                            <div>Pressure: ${weath.current.pressure} mb</div>
-                        </div>`   
-}
+// var test = start();
+// console.log(test);
 
 
-document.getElementById('iForm').addEventListener('submit', enterCity);
+
